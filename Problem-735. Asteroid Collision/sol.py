8efa1b3d190f44836c291003f68e7be93d6b3f23
Syed Yousha <syed.yousha23@gmail.com>
2026-01-26 13:39:03 +0500

    735. Asteroid Collision

---
+++ Problem-735. Asteroid Collision/sol.py
@@ -0,0 +1,19 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for a in asteroids:
+            while stack and a < 0 and stack[-1] > 0:
+                if stack[-1] < -a:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -a:
+                    stack.pop()
+                break
+            else:
+                stack.append(a)
+
+        return stack
+
+# time complexity is O(n)
+# space complexity is O(n)
