5c807237fb880f74d30f18435ce7965468d04331
Syed Yousha <syed.yousha23@gmail.com>
2026-02-13 09:36:16 +0500

    1071. Greatest Common Divisor of Strings

---
+++ Problem-1071. Greatest Common Divisor of Strings/sol.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        if str1+str2 != str2+str1:
+            return ""
+
+        div = math.gcd(len(str1), len(str2))
+
+        return str1[:div]
+
+        
+
+# time complexity: O(n)
+# space complexity: O(1)
